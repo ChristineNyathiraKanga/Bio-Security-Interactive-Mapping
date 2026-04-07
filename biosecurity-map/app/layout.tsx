b3277dba2eff4d3ca46a738cc41907aea08abae1
ChristineNyathiraKanga <nyathirakanga@gmail.com>
2026-04-07 16:08:22 +0300

    fix: add import statement for css

--- biosecurity-map/app/layout.tsx
+++ biosecurity-map/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
+
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Victory Farms - Biosecurity Site Map",
